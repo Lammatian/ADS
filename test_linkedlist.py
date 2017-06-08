fdd93b044d7bee2ee871146e5d2326605b8ba4b4
Mateusz Parafinski <mateusz.parafinski@gmail.com>
2017-06-08 23:34:46 +0100

    spacing

---
+++ test_linkedlist.py
@@ -0,0 +1,81 @@
+import sys
+import pytest
+sys.path.append('../') # to make folders with code visible
+
+from structures.linkedlist import *
+
+class TestClass(object):
+
+	def test_main(self):
+		l = LinkedList([1,2,3,4])
+
+		assert str(l) == "1 -> 2 -> 3 -> 4"
+		assert l.length == 4
+
+
+	def test_lookup(self):
+		l = LinkedList([1,2,3,4])
+
+		assert l.lookup(2) == 3
+		assert l.lookup(3) == 4
+
+		with pytest.raises(IndexError):
+			l.lookup(4)
+
+
+	def test_insert(self):
+		l = LinkedList([1,2,3,4])
+		l.insert(3, l.head)
+
+		assert str(l) == "1 -> 3 -> 2 -> 3 -> 4"
+		assert l.length == 5
+
+		l = LinkedList([1,2,3,4])
+		l.insert(3, l.head.next.next.next)
+
+		assert str(l) == "1 -> 2 -> 3 -> 4 -> 3"
+		assert l.length == 5
+
+
+	def test_insertFirst(self):
+		l = LinkedList([1,2,3,4])
+		l.insertFirst(0)
+
+		assert str(l) == "0 -> 1 -> 2 -> 3 -> 4"
+		assert l.length == 5
+
+
+	def test_remove(self):
+		l = LinkedList([1,2,3,4])
+		l.remove(l.head)
+
+		assert str(l) == "1 -> 3 -> 4"
+		assert l.length == 3
+
+		l = LinkedList([1,2,3,4])
+		l.remove(l.head.next.next.next)
+
+		assert str(l) == "1 -> 2 -> 3 -> 4"
+		assert l.length == 4
+
+
+	def test_removeFirst(self):
+		l = LinkedList([1,2,3,4])
+		l.removeFirst()
+
+		assert str(l) == "2 -> 3 -> 4"
+		assert l.length == 3
+
+
+	def test_delete(self):
+		l = LinkedList([1,2,3,4])
+		l.delete(1)
+
+		assert str(l) == "2 -> 3 -> 4"
+		assert l.length == 3
+
+		l = LinkedList([1,2,3,4])
+		l.delete(5)
+
+		assert str(l) == "1 -> 2 -> 3 -> 4"
+		assert l.length == 4
