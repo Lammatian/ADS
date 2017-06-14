8dc52a6bae6573beb826bf5d938617212bdb0320
Mateusz Parafinski <mateusz.parafinski@gmail.com>
2017-06-15 00:43:12 +0100

    Added tests for updated version of the array

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,16 +1,15 @@
 import sys
 import pytest
-sys.path.append('../') # to make folders with code visible
+sys.path.append('../')
 
-from structures.array import *
+from structures.array import Array
 
 class TestClass(object):
 
 	def test_main(self):
 		a = Array(3, [1,2])
 
-		assert str(a) == "[1, 2]"
-		assert a.length == 2
+		assert str(a) == "[1, 2, None]"
 
 
 	def test_lookup(self):
@@ -19,93 +18,25 @@ class TestClass(object):
 		assert a.lookup(0) == 1
 
 		with pytest.raises(IndexError):
-			a.lookup(2)
+			a.lookup(3)
 
 		with pytest.raises(IndexError):
 			a.lookup(-1)
-
-
-	def test_update(self):
-		a = Array(3, [1,2,3])
-		a.update(0, 4)
-
-		assert str(a) == "[4, 2, 3]"
-		assert a.length == 3
-
-		a = Array(3, [1,2,3])
-		a.update(2, 4)
-
-		assert str(a) == "[1, 2, 4]"
-
-		with pytest.raises(IndexError):
-			a.update(-1, 5)
-
-		with pytest.raises(IndexError):
-			a.update(3, 5)
 
 
 	def test_insert(self):
 		a = Array(3, [1,2])
 		a.insert(1, 5)
 
-		assert str(a) == "[1, 5, 2]"
-		assert a.length == 3
+		assert str(a) == "[1, 5, None]"
 
 		a = Array(3, [1,2,3])
-
-		with pytest.raises(ArrayFullError):
-			a.insert(1, 0)
 
 		with pytest.raises(IndexError):
 			a.insert(-1, 0)
 
 		with pytest.raises(IndexError):
 			a.insert(3, 0)
-
-
-	def test_insertLast(self):
-		a = Array(3, [1,2])
-		a.insertLast(3)
-
-		assert str(a) == "[1, 2, 3]"
-		assert a.length == 3
-
-		with pytest.raises(ArrayFullError):
-			a.insertLast(0)
-
-
-	def test_remove(self):
-		a = Array(3, [1,2,3])
-		a.remove(2)
-
-		assert str(a) == "[1, 2]"
-		assert a.length == 2
-
-		a.remove(1)
-
-		assert str(a) == "[1]"
-		assert a.length == 1
-
-		with pytest.raises(IndexError):
-			a.remove(1)
-
-
-	def test_removeLast(self):
-		a = Array(3, [1,2])
-		a.removeLast()
-
-		assert str(a) == "[1]"
-		assert a.length == 1
-
-		a.removeLast()
-
-		assert str(a) == "[]"
-		assert a.length == 0
-
-		a.removeLast()
-
-		assert str(a) == "[]"
-		assert a.length == 0
 
 
 	def test_find(self):
