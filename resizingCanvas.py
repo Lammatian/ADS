a295cdaf42272c227fcc05e908f52f1d14f904c6
Mark Sanders <ziplokk.mark.sanders@gmail.com>
2017-06-20 20:15:43 -0500

    Replace tabs with 4 spaces.
    Python3 will throw an error if tabs and spaces are mixed in the same file. The code so far has worked since the tabs and spaces are in different files, but it's best to maintain consistency and follow pep-8.
    
    See: https://www.python.org/dev/peps/pep-0008/#tabs-or-spaces

--- resizingCanvas.py
+++ resizingCanvas.py
@@ -1,42 +1,42 @@
 import tkinter as tk
 
 class ResizingCanvas(tk.Canvas):
-	"""
-	A subclass of Canvas for dealing with resizing windows
-	Taken from https://stackoverflow.com/questions/22835289/how-to-get-tkinter-canvas-to-dynamically-resize-to-window-width
-	"""
-	def __init__(self, parent, **kwargs):
-		tk.Canvas.__init__(self, parent, **kwargs)
-		self.bind("<Configure>", self.on_resize)
-		self.height = self.winfo_reqheight()
-		self.width = self.winfo_reqwidth()
+    """
+    A subclass of Canvas for dealing with resizing windows
+    Taken from https://stackoverflow.com/questions/22835289/how-to-get-tkinter-canvas-to-dynamically-resize-to-window-width
+    """
+    def __init__(self, parent, **kwargs):
+        tk.Canvas.__init__(self, parent, **kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
 
 
-	def on_resize(self, event):
-		"""Resize all elements of canvas on resize of the window"""
-		# determine the ratio of old width/height to new values
-		wscale = float(event.width)/self.width
-		hscale = float(event.height)/self.height
-		self.width = event.width
-		self.height = event.height
-		# resize the canvas
-		self.config(width=self.width, height=self.height)
-		# resize all the objects tagged with "all" tag (by addtag_all method)
-		self.scale("all", 0, 0, wscale, hscale)
+    def on_resize(self, event):
+        """Resize all elements of canvas on resize of the window"""
+        # determine the ratio of old width/height to new values
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas
+        self.config(width=self.width, height=self.height)
+        # resize all the objects tagged with "all" tag (by addtag_all method)
+        self.scale("all", 0, 0, wscale, hscale)
 
 def main():
-	root = tk.Tk()
-	myframe = tk.Frame(root)
-	myframe.pack(fill="both", expand="yes")
-	mycanvas = ResizingCanvas(myframe, width=850, height=400, bg="white", highlightthickness=0)
-	mycanvas.pack(fill="both", expand="yes")
+    root = tk.Tk()
+    myframe = tk.Frame(root)
+    myframe.pack(fill="both", expand="yes")
+    mycanvas = ResizingCanvas(myframe, width=850, height=400, bg="white", highlightthickness=0)
+    mycanvas.pack(fill="both", expand="yes")
 
-	mycanvas.create_line(0, 0, 200, 100)
-	mycanvas.create_line(0, 100, 200, 0, fill="red", dash=(4,4))
-	mycanvas.create_rectangle(50, 25, 150, 75, fill="blue")
+    mycanvas.create_line(0, 0, 200, 100)
+    mycanvas.create_line(0, 100, 200, 0, fill="red", dash=(4,4))
+    mycanvas.create_rectangle(50, 25, 150, 75, fill="blue")
 
-	mycanvas.addtag_all("all")
-	root.mainloop()
+    mycanvas.addtag_all("all")
+    root.mainloop()
 
 if __name__ == "__main__":
-	main()
+    main()
--- structures/queue.py
+++ structures/queue.py
@@ -4,50 +4,50 @@ sys.path.append('../')
 from structures.dlinkedlist import *
 
 class Queue(object):
-	"""
-	Implementation of Queue using doubly linked list
+    """
+    Implementation of Queue using doubly linked list
 
-	Includes basic operations such as isEmpty, enqueue, dequeue, peek
-	"""
-	_title = "Queue"
+    Includes basic operations such as isEmpty, enqueue, dequeue, peek
+    """
+    _title = "Queue"
 
-	def __init__(self):
-		"""Initialise an empty queue"""
-		self._queue = DLinkedList()
-		self._front = None
-
-
-	def __repr__(self):
-		"""Representation of the queue"""
-		return super(Queue, self).__repr__()
+    def __init__(self):
+        """Initialise an empty queue"""
+        self._queue = DLinkedList()
+        self._front = None
 
 
-	def __str__(self):
-		"""Return str(self)"""
-		return repr(self)
+    def __repr__(self):
+        """Representation of the queue"""
+        return super(Queue, self).__repr__()
 
 
-	def isEmpty(self):
-		"""Check if the queue is empty"""
-		return self._queue.length == 0
+    def __str__(self):
+        """Return str(self)"""
+        return repr(self)
 
 
-	def peek(self):
-		"""Returns the first element in the queue"""
-		return self._front
+    def isEmpty(self):
+        """Check if the queue is empty"""
+        return self._queue.length == 0
 
 
-	def enqueue(self, n):
-		"""Append an element at the end of the queue"""
-		if self.isEmpty():
-			self._front = n
-		
-		self._queue.insertLast(n)
+    def peek(self):
+        """Returns the first element in the queue"""
+        return self._front
 
 
-	def dequeue(self):
-		"""Remove the front element from the queue and return it"""
-		last_front = self._front
-		self._queue.removeFirst()
-		self._front = self._queue.lookup(0) if self._queue.head else None
-		return last_front
+    def enqueue(self, n):
+        """Append an element at the end of the queue"""
+        if self.isEmpty():
+            self._front = n
+        
+        self._queue.insertLast(n)
+
+
+    def dequeue(self):
+        """Remove the front element from the queue and return it"""
+        last_front = self._front
+        self._queue.removeFirst()
+        self._front = self._queue.lookup(0) if self._queue.head else None
+        return last_front
