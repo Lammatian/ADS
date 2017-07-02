2c66fd60ba82f508e3c1d79e94fa49d574bee6f7
Lammatian <mateusz.parafinski@gmail.com>
2017-07-02 22:24:29 +0200

    Added lookup animation

--- structures/anim_linkedlist.py
+++ structures/anim_linkedlist.py
@@ -25,28 +25,31 @@ class A_LinkedList(LinkedList):
 
         # graphical representation of the linked list
         self.canvas = canvas
-        self.graphic = []        
+        self.nodes = []
+        self.arrows = []        
 
         for i, node in enumerate(self):
             # node
-            self.canvas.create_rectangle(100*i+50,\
+            rect = self.canvas.create_rectangle(100*i+50,\
                                          self.canvas.winfo_reqheight()//2-25,\
                                          100*i+100,\
                                          self.canvas.winfo_reqheight()//2+25,\
                                          fill="white")
             # value in the node
-            self.canvas.create_text(100*i+75,\
+            text = self.canvas.create_text(100*i+75,\
                                     self.canvas.winfo_reqheight()//2,\
                                     text=str(node.val))
+            self.nodes.append((rect, text))
 
             if i < self.length-1:
                 # arrow between nodes
-                self._draw_arrow(100*i+100, self.canvas.winfo_reqheight()//2)
+                arrow = self._draw_arrow(100*i+100, self.canvas.winfo_reqheight()//2)
+                self.arrows.append(arrow)
 
 
     def lookup(self, index):
         """Call lookup of linkedlist and animation"""
-        self._lookup_animation(index)
+        self._lookup_animation(index, 0, 0)
         
         return super(A_LinkedList, self).lookup(index)
 
@@ -55,7 +58,7 @@ class A_LinkedList(LinkedList):
         """Call insert of linkedlist and animation"""
         self._insert_animation(value, node)
         
-        super(A_LinkedList, self).insert(value, node)
+        super(A_LinkedList, self).insert(value, self._nodeAt(node))
 
 
     def insertFirst(self, value):
@@ -86,9 +89,26 @@ class A_LinkedList(LinkedList):
         super(A_LinkedList, self).delete(value)
 
 
-    def _lookup_animation(self, index):
+    def _lookup_animation(self, index, n, step):
         """Animation of the lookup operation"""
-        pass
+        if n < index:
+            if step == 0:
+                self._swap_color("rect", n, "yellow")
+                self.canvas.after(250, self._lookup_animation, index, n, 1)
+            elif step == 1:
+                self._swap_color("rect", n, "white")
+                self._swap_color("arr", n, "yellow")
+                self.canvas.after(250, self._lookup_animation, index, n, 2)
+            elif step == 2:
+                self._swap_color("arr", n, "black")
+                self.canvas.after(250, self._lookup_animation, index, n+1, 0)
+        else:
+            if step == 0:
+                self._swap_color("rect", n, "green")
+                self.canvas.after(500, self._lookup_animation, index, n, 1)
+            elif step == 1:
+                self._swap_color("rect", n, "white")
+                return
 
 
     def _insert_animation(self, value, node):
@@ -118,8 +138,32 @@ class A_LinkedList(LinkedList):
 
     def _draw_arrow(self, start_x, start_y):
         """Draw an arrow from one place to another"""
-        self.canvas.create_line((start_x, start_y), (start_x+50, start_y))
-        self.canvas.create_polygon((start_x+37, start_y+5), (start_x+50, start_y), (start_x+37, start_y-5))
+        line = self.canvas.create_line((start_x, start_y), (start_x+50, start_y))
+        poly = self.canvas.create_polygon((start_x+37, start_y+5), (start_x+50, start_y), (start_x+37, start_y-5))
+        return (line, poly)
+
+
+    def _swap_color(self, shape, index, color):
+        """Swap a color of a shape at given index"""
+        if shape == "rect":
+            self.canvas.itemconfig(self.nodes[index][0], fill=color)
+        elif shape == "arr":
+            self.canvas.itemconfig(self.arrows[index][0], fill=color)
+            self.canvas.itemconfig(self.arrows[index][1], fill=color)
+
+
+    def _nodeAt(self, index):
+        """
+        Returns node at given index
+        Necessary to call super methods
+        """
+        node = self.head
+
+        while index > 0:
+            node = node.next
+            index -= 1
+
+        return node
 
 
     animations = [
